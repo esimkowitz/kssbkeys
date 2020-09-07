705daa1c4c40b0e47f0497b4babf4879bdb61469
Evan Simkowitz <esimkowitz@wustl.edu>
2020-09-07 12:06:33 -0700

    adding new command to notify when dismissing notifications. hide commands from command palette

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,16 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const newFolder = vscode.commands.registerCommand('kssbkeys.explorerNewFolder', () => {
-        vscode.window.showInformationMessage("Enter folder name.");
+        vscode.window.showInformationMessage("Enter folder name");
 		vscode.commands.executeCommand("explorer.newFolder");
 	});
 
-	context.subscriptions.push(newFolder);
+	const dismissNotification = vscode.commands.registerCommand('kssbkeys.dismissNotification', () => {
+        vscode.window.showInformationMessage("Notification dismissed");
+		vscode.commands.executeCommand("notifications.hideToasts");
+	});
+
+	context.subscriptions.push(newFolder, dismissNotification);
 }
 
 export function deactivate() {}
