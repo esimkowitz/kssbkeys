7cb108907a6de84f28143a9096efcf13b6baa2ef
Evan Simkowitz <esimkowitz@wustl.edu>
2020-09-07 12:37:24 -0700

    fixing indentation

--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,12 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 	const newFolder = vscode.commands.registerCommand('kssbkeys.explorerNewFolder', () => {
-        vscode.window.showInformationMessage("Enter folder name");
+		vscode.window.showInformationMessage("Enter folder name");
 		vscode.commands.executeCommand("explorer.newFolder");
 	});
 
 	const dismissNotification = vscode.commands.registerCommand('kssbkeys.dismissNotification', () => {
-        vscode.window.showInformationMessage("Notification dismissed");
+		vscode.window.showInformationMessage("Notification dismissed");
 		vscode.commands.executeCommand("notifications.hideToasts");
 	});
 
